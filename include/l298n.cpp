9a5256f28fe64bdf63a0ef17624b727cc03ee7aa
Sebastian Muchui <Sebastianmuchui79@gmail.com>
2022-08-04 14:59:37 +0300

    Update movement functions

--- include/l298n.cpp
+++ include/l298n.cpp
@@ -8,22 +8,68 @@ L298N::L298N(int a, int b,int c){
     for(int pin = 0; pin<3; pin++){
         pinMode(enable_pins[pin],OUTPUT);
     }    
+    
+    setSpeed(NULL);
 };
 
-void L298N::on(){
-    digitalWrite(this->enablePin,HIGH);
-};
-
-void L298N::off(){
-    digitalWrite(this->enablePin,LOW);
-};
-
-void L298N::forward(){
-    digitalWrite(this->in1, HIGH); 
-    digitalWrite(this->in2, LOW);
+float L298N::setSpeed(float n){
+    if(n != NULL){
+        this->motorSpeed = 255;       
+    }else{
+        this->motorSpeed = map(n,0,100,0,255);
+    }
+    return motorSpeed;
 }
 
+float L298N::readSpeed(){
+    float speed = analogRead(this->enablePin);
+    return speed;
+}
+
+bool L298N::isOn(){
+    if(digitalRead(this->enablePin) == HIGH || analogRead(this->enablePin > 0)){
+        return true;
+    }else{
+        return false;
+    }
+}
+bool L298N::isOff(){
+    if(!isOn()){
+        return true;
+    }else{
+        return false;
+    }
+}
+void L298N::on(){
+    if(isOn() == false){
+        digitalWrite(this->enablePin,HIGH);         
+    }else{
+        // Do nothing
+    }
+};
+void L298N::off(){
+    if(isOn()){
+        digitalWrite(this->enablePin,LOW);        
+    }
+};
+void L298N::forward(){
+    if(isOn()){
+        digitalWrite(this->in1, HIGH); 
+        digitalWrite(this->in2, LOW);
+    }else{
+        // Turn on enable pin then move forward
+        on();
+        delay(100);
+        forward();
+    }
+}
 void L298N::backward(){
-    digitalWrite(this->in1, LOW); 
-    digitalWrite(this->in2, HIGH);
+    if(isOn()){
+        digitalWrite(this->in1, LOW); 
+        digitalWrite(this->in2, HIGH);        
+    }else{
+        on();
+        delay(100);
+        backward();
+    }
 }
